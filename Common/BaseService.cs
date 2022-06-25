ecbe147409d8b5891fd61f67e4bfffaa4873b898
Jibranwafi <joystick157725@gmail.com>
2022-06-26 00:51:01 +0700

    adds common, data/migrations, infrastructure

---
+++ Common/BaseService.cs
@@ -0,0 +1,9 @@
+﻿using Iuli.Cse19.CarRental.WebApp.Infrastructure;
+
+namespace Iuli.Cse19.CarRental.WebApp.Common
+{
+    public class BaseService
+    {
+        protected IAppDbContext ApplicationDbContext { get; set; }
+    }
+}
---
+++ Common/IAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Iuli.Cse19.CarRental.WebApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Iuli.Cse19.CarRental.WebApp.Common
+{
+    public interface IAppDbContext
+    {
+        int SaveChanges();
+        EntityEntry Remove([NotNull] object entity);
+
+        DbSet<Customer> Customer { get; set; }
+
+        DbSet<Owner> Owner { get; set; }
+        DbSet<Car> Car { get; set; }
+
+        DbSet<RentedCarInformation> RentInfo { get; set; }
+
+    }
+}
